a9a51a73d0efa0dd14d3a40d9c2286ec0966ff17
hackeruser240 <afaqahmad240@yahoo.com>
2025-05-23 03:24:47 +0500

    divided savetoPDF() into subfunctions for easy control

--- scripts/savetoPDF.py
+++ scripts/savetoPDF.py
@@ -1,40 +1,47 @@
-import sys,os
+import os
 
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
 
+def title(var):
+    maintitle=f"'{var.repo}' details"
+    var.c.setFont("Helvetica-Bold", 16)
+    main_title_width = var.c.stringWidth(maintitle, "Helvetica-Bold", 16)
+    var.c.drawString((var.width - main_title_width) / 2, var.height - 1 * inch, maintitle)
+
+def commit_titles(var):
+    if var.commit_titles==[]:
+        pass
+    else:
+        sub_title = "Commit Titles:"
+        var.c.setFont("Helvetica-Bold", 14)
+        var.c.stringWidth(sub_title, "Helvetica-Bold", 14)
+        var.c.drawString(1*inch, var.height - 1.3 * inch, sub_title)
+
+        var.c.setFont("Helvetica", 10)
+        y = var.height - 1.6 * inch
+
+        for i, msg in enumerate(var.commit_titles, start=1):
+            text = f"{i}. {msg}"
+            var.c.drawString(1 * inch, y, text)
+            y -= 0.25 * inch  # adjust spacing
+
+            if y < 1 * inch:
+                var.c.showPage()  # new page
+                y = var.height - 1 * inch
+            var.c.setFont("Helvetica", 10)
+
 def save_to_PDF(var):
     os.makedirs(os.path.dirname(var.save_dir), exist_ok=True)
     var.save_dir=os.path.join( os.path.dirname(var.save_dir) ,"Commit Titles.pdf")
+    var.c = canvas.Canvas(var.save_dir, pagesize=letter)
+    var.width, var.height = letter
 
-    c = canvas.Canvas(var.save_dir, pagesize=letter)
-    width, height = letter
     
-    maintitle=f"'{var.repo}' details"
-    c.setFont("Helvetica-Bold", 16)
-    main_title_width = c.stringWidth(maintitle, "Helvetica-Bold", 16)
-    c.drawString((width - main_title_width) / 2, height - 1 * inch, maintitle)
+    title(var)
+    commit_titles(var)
 
-    sub_title = "Commit Titles:"
-    c.setFont("Helvetica-Bold", 14)
-    c.stringWidth(sub_title, "Helvetica-Bold", 14)
-    #sub_title_width = c.stringWidth(sub_title, "Helvetica", 14)
-    c.drawString(1*inch, height - 1.3 * inch, sub_title)
-
-    c.setFont("Helvetica", 10)
-    y = height - 1.6 * inch
-
-    for i, msg in enumerate(var.commit_titles, start=1):
-        text = f"{i}. {msg}"
-        c.drawString(1 * inch, y, text)
-        y -= 0.25 * inch  # adjust spacing
-
-        if y < 1 * inch:
-            c.showPage()  # new page
-            y = height - 1 * inch
-            c.setFont("Helvetica", 10)
-
-    c.save()
+    var.c.save()
     
     print(f"PDF saved at: {var.save_dir}")
